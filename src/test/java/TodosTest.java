f7f56ba62af4502440da8e908d99d01f6ea6a196
Dmitriz1 <stud0000264980@study.utmn.ru>
2024-10-28 12:41:21 +0500

    task completed 3

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -4,12 +4,31 @@ import org.junit.jupiter.api.Test;
 
 public class TodosTest {
     @Test
-    public void shouldFindThreeTasksOfDifferentType() {
+    public void shouldFindSimpleTask() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        Todos todos = new Todos();
 
+        todos.add(simpleTask);
+
+        Task[] expected1 = {simpleTask};
+        Task[] actual1 = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected1, actual1);
+    }
+
+    @Test
+    public void shouldFindEpicTask() {
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
         Epic epic = new Epic(55, subtasks);
 
+        Todos todos = new Todos();
+
+        Task[] expected2 = {epic};
+        Task[] actual2 = todos.search("Яйца");
+        Assertions.assertArrayEquals(expected2, actual2);
+    }
+
+    @Test
+    public void shouldFindMeeting() {
         Meeting meeting = new Meeting(
                 555,
                 "Выкатка 3й версии приложения",
@@ -19,23 +38,11 @@ public class TodosTest {
 
         Todos todos = new Todos();
 
-        todos.add(simpleTask);
-        todos.add(epic);
         todos.add(meeting);
-
-        Task[] expected1 = {simpleTask};
-        Task[] actual1 = todos.search("Позвонить");
-        Assertions.assertArrayEquals(expected1, actual1);
-
-        Task[] expected2 = {epic};
-        Task[] actual2 = todos.search("Яйца");
-        Assertions.assertArrayEquals(expected2, actual2);
-
         Task[] expected3 = {meeting};
         Task[] actual3 = todos.search("НетоБанка");
         Assertions.assertArrayEquals(expected3, actual3);
     }
-
     @Test
     public void shouldFindNoTasksIfNoMatch() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
@@ -70,7 +77,7 @@ public class TodosTest {
     }
 
     @Test
-    public void shouldFindSeveralTasks() {
+    public void shouldFindAllTasks() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
         Todos todos = new Todos();
         String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
@@ -94,5 +101,25 @@ public class TodosTest {
         Task[] expected2 = {meeting};
         Task[] actual2 = todos.search("Приложение НетоБанка");
         Assertions.assertArrayEquals(expected2, actual2);
+    }
+
+    @Test
+    public void shouldFindSeveralTasks() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        Todos todos = new Todos();
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Позвонить в НетоБанк",
+                "Во вторник после обеда"
+        );
+
+        todos.add(simpleTask);
+        todos.add(meeting);
+
+        Task[] expected1 = {simpleTask, meeting};
+        Task[] actual1 = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected1, actual1);
     }
 }
